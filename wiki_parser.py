4945bdc3d16c1419e7a7dc6f8f58174f1ced555c
MangriMen <mangrimen@gmail.com>
2021-07-08 19:40:17 +0700

    Fixed typo in wiki link

--- wiki_parser.py
+++ wiki_parser.py
@@ -26,9 +26,9 @@ def creation_date(path_to_file):
 
 
 def get_data_from_site(main):
-    site = 'https://escapefromTRND.fandom.com'
-    url_weapons = 'https://escapefromTRND.fandom.com/ru/wiki/%D0%9E%D1%80%D1%83%D0%B6%D0%B8%D0%B5'
-    url_mods = 'https://escapefromTRND.fandom.com/ru/wiki/%D0%9E%D1%80%D1%83%D0%B6%D0%B5%D0%B9%D0%BD%D1%8B%D0%B5_' \
+    site = 'https://escapefromtarkov.fandom.com'
+    url_weapons = 'https://escapefromtarkov.fandom.com/ru/wiki/%D0%9E%D1%80%D1%83%D0%B6%D0%B8%D0%B5'
+    url_mods = 'https://escapefromtarkov.fandom.com/ru/wiki/%D0%9E%D1%80%D1%83%D0%B6%D0%B5%D0%B9%D0%BD%D1%8B%D0%B5_' \
                '%D1%87%D0%B0%D1%81%D1%82%D0%B8_%D0%B8_%D0%BC%D0%BE%D0%B4%D1%8B '
 
     min_s = 0
