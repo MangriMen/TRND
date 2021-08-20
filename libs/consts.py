82a7b8e5b27f4cf8321f66a3ea08790c88bc6bac
MangriMen <49300253+MangriMen@users.noreply.github.com>
2021-08-21 06:39:08 +0700

    Added .py file with constants

---
+++ libs/consts.py
@@ -0,0 +1,36 @@
+GITHUB_LINK_RELEASES = "https://github.com/MangriMen/TRND/releases"
+GITHUB_API_LINK_RELEASES = "https://api.github.com/repos/MangriMen/TRND/releases"
+
+PROGRAM_NAME = "TRND"
+VERSION = "0.17"
+
+PROGRAM_FOLDER_WIN = "TRND"
+PROGRAM_FOLDER_LINUX = ".trnd"
+PARENT_FOLDER_ENV_WIN = "APPDATA"
+PARENT_FOLDER_ENV_LINUX = "HOME"
+LOGS_FOLDER = "logs"
+
+DATA_FILE = "data.json"
+
+LOGS_FILE_STRFTIME = "%Y-%m-%d_%H-%M-%S"
+LOGS_FILE_EXTENSION = ".log"
+
+PARTS_DATE_STRFTIME = "%d.%m.%Y %X"
+LOGGER_FORMAT_STR = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+RESOURCE_FOLDER = "data"
+STYLE_FILE = "additional_style.css"
+ICON_FILE = "icon.ico"
+MAIN_WINDOW_UI_FILE = "main_window.ui"
+TRANSLATOR_FILE = "qtbase_ru.qm"
+FONT_FILE = "Roboto-Regular.ttf"
+THEME_FILE = "tarkov_theme.xml"
+
+LOGS_MAX_COUNT = 10
+UPDATE_TIMEOUT_SECONDS = 60
+
+DATA_WEAPONS_KEY = 'weapons'
+DATA_MODS_KEY = 'mods'
+DATA_MODS_CONFLICTS_KEY = 'modsConflicts'
+DATA_WEAPONS_LAST_UPDATE_KEY = "weaponsLastUpdate"
+DATA_MODS_LAST_UPDATE_KEY = "modsLastUpdate"
