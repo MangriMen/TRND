ec9c011144547d3b265a993035bc6bc11eb960fe
MangriMen <49300253+MangriMen@users.noreply.github.com>
2024-03-22 04:01:38 +0700

    Add test graphql request

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,6 +1,29 @@
-import { Button } from '@/shared/ui';
 import './app.css';
 
+import { gql } from 'graphql-request';
+import { createResource } from 'solid-js';
+
+import { Button } from '@/shared/ui';
+
+import { runTarkovQuery } from '@/entities/tarkov';
+
 export const AppRouter = () => {
-  return <Button>Hello</Button>;
+  const [data] = createResource(() =>
+    runTarkovQuery(gql`
+      {
+        items(name: "m855a1") {
+          id
+          name
+          shortName
+        }
+      }
+    `),
+  );
+
+  return (
+    <div>
+      <div>{JSON.stringify(data())}</div>
+      <Button>Hello</Button>
+    </div>
+  );
 };
